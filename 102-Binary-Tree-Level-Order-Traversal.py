cb3e73741e4064dcc12651a9fbe2e05436a15122
Piyush Chavan <38141478+ppchavan@users.noreply.github.com>
2024-07-08 20:55:46 -0700

    [102] [Time Beats: 89.67%] [Memory Beats: 8.91%] - LeetPush

---
+++ 102-Binary-Tree-Level-Order-Traversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        
+        # Breadth first search approach (BFS) explained in https://www.youtube.com/watch?v=6ZnyEApgFYg
+        res = []        
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            q_len = len(q)
+            level = []
+            # Process through entire level
+            for i in range(q_len):
+                n = q.popleft()
+
+                if n:
+                    level.append(n.val)
+                    if n.left:
+                        q.append(n.left)
+                    if n.right:
+                        q.append(n.right)
+            # After processing level, add it to result
+            if level:
+                res.append(level)
+        return res
+
+        
+        
