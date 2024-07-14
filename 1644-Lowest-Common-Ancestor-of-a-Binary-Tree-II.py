6c434aa2cef5b8b443a8025863bcc5ac56f0b24d
Piyush Chavan <38141478+ppchavan@users.noreply.github.com>
2024-07-13 18:56:11 -0700

    [1644] [Time Beats: 53.27%] [Memory Beats: 15.33%] - LeetPush

---
+++ 1644-Lowest-Common-Ancestor-of-a-Binary-Tree-II.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """ 
+        These pfound and qfound flags are required since its not guaranteed if p and q are both present in given tree. 
+        If one of them is not present in entire tree, we need to return None.
+        """
+        pfound = False
+        qfound = False
+
+        def dfs(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+            if not root:
+                return None
+            
+            # Imp: to declare these flags as nonlocal. Otherwise the values are only modified "locally". But not passed outside of scope of recursive function.
+            nonlocal pfound
+            nonlocal qfound
+            l = dfs(root.left, p, q)
+            r = dfs(root.right, p, q)
+
+            if root == p or root == q:
+                if root == p:
+                    pfound = True
+                else:
+                    qfound = True
+                return root
+
+            if l and r:
+                return root
+            else:
+                return l or r
+
+        ans = dfs(root, p, q)
+
+        # Return ans only if both p and q were found, otherwise return None (since one of the p,q nodes was absent).
+        return ans if pfound and qfound else None
+
+# Time complexity is O(N) since we are doing a single traversal for entire tree
