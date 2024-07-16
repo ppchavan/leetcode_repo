a9430a6984efbd0513d6320cc17eb224b684c9e0
Piyush Chavan <38141478+ppchavan@users.noreply.github.com>
2024-07-15 18:37:11 -0700

    [153] [Time Beats: 95.64%] [Memory Beats: 10.60%] - LeetPush

---
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+        
+        res = nums[0]
+        l, r = 0, len(nums)-1
+        
+        # We use binary search approach. Iterate through the array, make sure that left and right pointers don't cross. But they can be same element (mid).
+        while l <= r:
+            mid = (l + r)// 2
+            # Keep track of min at each iteration.
+            res = min(res, nums[mid])     
+            
+            # Check if middle element is greater than or equal to right element. If yes, search right.
+            # Otherwise search in the left side interval.
+            if nums[mid] >= nums[r]:
+                # search right interval                
+                l = mid + 1
+            else:
+                # search left interval
+                r = mid - 1            
+        
+        return res
+                
+# Since we are using binary search approach, the time complexity is O(logN) 
