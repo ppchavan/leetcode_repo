12aa9c5f92ef5826861609c96156266b5f7c789d
Piyush Chavan <38141478+ppchavan@users.noreply.github.com>
2024-07-24 20:20:12 -0700

    [523] [Time Beats: 76.14%] [Memory Beats: 40.39%] - LeetPush

--- 523-Continuous-Subarray-Sum.py
+++ 523-Continuous-Subarray-Sum.py
@@ -5,16 +5,21 @@ class Solution:
         
         # Explanation: https://www.youtube.com/watch?v=OKcrLfR-8mE
         cum_sum = 0
+        # This is a map of remainder to index, by computing cumulative sum and storing remainder = sum % k
+        # We need to store index i here as value in dictionary because the condition is that subarray length has to be at least 2.
         remainder = {0: -1}
 
         for i, n in enumerate(nums):
             cum_sum += n
             r = cum_sum % k
+            # This is a new entry in the map since key is not found in map
             if r not in remainder:
                 remainder[r] = i
+            # Key was found, so check if subarray length is greater than 1 (at least 2 elements long)
             elif i - remainder[r] > 1:
                 return True
         
+        # If solution not found after full iteration, means there is no subarray sum which is multiple of k
         return False           
 
 # Time complexity: O(N), Since we iterate over whole array only once. Lookup in dictionary is constant time.
